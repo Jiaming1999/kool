aab54dfd3424f0bc50d5eaf57b32023b0b64737a
Jiaming <qq1104382148@gmail.com>
2021-11-07 16:35:20 -0600

    db flaky test fix

--- tests/contrib/quizzes/test_question.py
+++ tests/contrib/quizzes/test_question.py
@@ -7,7 +7,8 @@ from kool.contrib.auth import User
 from kool.contrib.courses import Course
 from kool.contrib.quizzes import Quiz, Question
 
-TESTS_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+TESTS_DIR = os.path.dirname(os.path.dirname(
+    os.path.dirname(os.path.abspath(__file__))))
 
 
 class TestQuestion(object):
@@ -16,17 +17,18 @@ class TestQuestion(object):
     def setup_class(cls):
         testdb = os.path.join(TESTS_DIR, 'fixtures')
         Model.db = FlatFileDB(path=testdb)
-        cls.user = User(first_name='John', last_name='Doe', 
-            email='john@doe.com', password='secretpwd')
+        cls.user = User(first_name='John', last_name='Doe',
+                        email='john@doe.com', password='secretpwd')
         cls.user_id = cls.user.save()
         cls.course_id = Course('Testers', cls.user_id).save()
         cls.quiz = Quiz('quiz test', cls.course_id)
         cls.quiz_id = cls.quiz.save()
         cls.question = Question('favourite pet?', cls.quiz_id)
-        cls.question_id = cls.question.save()    
+        cls.question_id = cls.question.save()
 
     def test_add_choices(self):
-        self.question.add_choices(choices=['cat', 'dog', 'bird'], correct_answers=['dog'])
+        self.question.add_choices(
+            choices=['cat', 'dog', 'bird'], correct_answers=['dog'])
         assert self.question.choices == {1: 'cat', 2: 'dog', 3: 'bird'}
         assert self.question.correct_answers == [(2, 'dog')]
 
@@ -41,6 +43,3 @@ class TestQuestion(object):
     def test_check_answer(self):
         response = self.question.check_answer()
         assert response == {2: 'Correct'}
-
-
-
